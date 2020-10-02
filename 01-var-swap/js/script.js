1a6c51d6569ced45891807dfd9326566e0cbd694
[Students] Edvards Damnieks <92DPEDamnieks@rvt.lv>
2020-10-02 14:46:53 +0300

    .

---
+++ 01-var-swap/js/script.js
@@ -0,0 +1,16 @@
+console.log("Sveiciens no konsoles!")
+
+let mainīgais1 = "Viens"
+let mainīgais2 = "Divi"
+
+console.log("mainīgais1:", mainīgais1)
+console.log("mainīgais2:", mainīgais2)
+
+
+let temp = mainīgais1
+mainīgais1 = mainīgais2
+mainīgais2 = temp
+
+
+console.log("mainīgais1:", mainīgais1)
+console.log("mainīgais2:", mainīgais2)
